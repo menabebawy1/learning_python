8aa55be80a083552818ad0f40e7a189fa8c3a763
menabebawy1 <mena.m.bebawy@gmail.com>
2020-10-26 02:54:12 -0400

    more basics

--- file.py
+++ file.py
@@ -11,3 +11,31 @@ y = "hello"
 
 print(x)
 print(y)
+
+x = int(5)
+print(type(x)) #I am a comment
+
+
+#This is called casting
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1]) #banana
+print(thislist[-1]) #cherry
